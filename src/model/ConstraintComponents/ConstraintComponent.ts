04ee913d497c87714caaeaa1a9e0b1e13f325e8a
Hans-Christiaan <hans-christiaan@yoast.com>
2021-08-14 21:14:29 +0200

    Added doc blocks to the ConstraintComponent class.

--- src/model/ConstraintComponents/ConstraintComponent.ts
+++ src/model/ConstraintComponents/ConstraintComponent.ts
@@ -1,7 +1,33 @@
 import { IPropertyShape } from "../Shapes/PropertyShape";
 import { Node } from "../Node";
 
+/**
+ * A constraint component.
+ *
+ * Represents a possible constraint on a linked data node's properties.
+ *
+ * @see https://www.w3.org/TR/shacl/#dfn-constraint-component
+ */
 export default abstract class ConstraintComponent {
-	abstract isValid( node: Node, property: IPropertyShape ): boolean;
-	abstract getMessage( node: Node, property: IPropertyShape ): string;
+	/**
+	 * Checks whether the given node is valid according to the rules of this
+	 * constraint component and the configuration given in the property shape.
+	 *
+	 * @param node The node to check.
+	 * @param propertyShape The property shape to use.
+	 *
+	 * @return Whether the node is valid according to this constraint and the given property shape.
+	 */
+	abstract isValid( node: Node, propertyShape: IPropertyShape ): boolean;
+
+	/**
+	 * Returns the message that should be used when this constraint is violated
+	 * for the given node and the configuration in given property shape.
+	 *
+	 * @param node The node to check.
+	 * @param propertyShape The property shape to use.
+	 *
+	 * @return The message that should be used when this constraint is violated.
+	 */
+	abstract getMessage( node: Node, propertyShape: IPropertyShape ): string;
 }
